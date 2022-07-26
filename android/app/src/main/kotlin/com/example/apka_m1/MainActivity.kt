be96f95fb62c2ad90792600cd9763806f41fffc5
ShiroganeS <saito.shirogane@gmail.com>
2022-07-26 18:45:01 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/apka_m1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apka_m1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,404 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const PierwszyEkran(),
+    );
+  }
+}
+
+class PierwszyEkran extends StatelessWidget {
+  const PierwszyEkran({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Start"),
+        centerTitle: true,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/las.jpg"),
+            opacity: 0.7,
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage("images/D.jpg"),
+              radius: 100,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Dziubas Developer",
+              style: GoogleFonts.greatVibes(
+                  fontSize: 40,
+                  color: const Color.fromARGB(246, 0, 94, 47),
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.favorite),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => const Ekran3(),
+                      ),
+                    );
+                  },
+                  color: Colors.pink,
+                  iconSize: 40.0,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.audiotrack),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => const Ekran4(),
+                      ),
+                    );
+                  },
+                  color: const Color.fromARGB(255, 217, 233, 72),
+                  iconSize: 30.0,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.beach_access),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => const Ekran5(),
+                      ),
+                    );
+                  },
+                  color: Colors.blue,
+                  iconSize: 36.0,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => const Ekran2(),
+                  ),
+                );
+              },
+              child: const Text("2 ekran"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Ekran2 extends StatelessWidget {
+  const Ekran2({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: const Text("Powrót"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/las.jpg"),
+              opacity: 0.7,
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Dziubas Developer",
+                style: GoogleFonts.greatVibes(
+                    fontSize: 40,
+                    color: const Color.fromARGB(246, 0, 94, 47),
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Ekran3 extends StatelessWidget {
+  const Ekran3({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: const Text("Powrót"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/las.jpg"),
+              opacity: 0.7,
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Dziubas Developer",
+                style: GoogleFonts.greatVibes(
+                    fontSize: 40,
+                    color: const Color.fromARGB(246, 235, 22, 15),
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Ekran4 extends StatelessWidget {
+  const Ekran4({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: const Text("Powrót"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/las.jpg"),
+              opacity: 0.7,
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Dziubas Developer",
+                style: GoogleFonts.greatVibes(
+                    fontSize: 40,
+                    color: const Color.fromARGB(246, 196, 230, 46),
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Ekran5 extends StatelessWidget {
+  const Ekran5({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: const Text("Powrót"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/las.jpg"),
+              opacity: 0.7,
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Dziubas Developer",
+                style: GoogleFonts.greatVibes(
+                    fontSize: 40,
+                    color: const Color.fromARGB(246, 36, 115, 233),
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Ekran6 extends StatelessWidget {
+  const Ekran6({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: const Text("Powrót"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/las.jpg"),
+              opacity: 0.7,
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Dziubas Developer",
+                style: GoogleFonts.greatVibes(
+                    fontSize: 40,
+                    color: const Color.fromARGB(246, 0, 94, 47),
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Ekran7 extends StatelessWidget {
+  const Ekran7({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: const Text("Powrót"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/las.jpg"),
+              opacity: 0.7,
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Dziubas Developer",
+                style: GoogleFonts.greatVibes(
+                    fontSize: 40,
+                    color: const Color.fromARGB(246, 0, 94, 47),
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
