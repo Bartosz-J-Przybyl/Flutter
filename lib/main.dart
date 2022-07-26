85d111afff0d84d628825e9b0e3f1f056500be48
ShiroganeS <saito.shirogane@gmail.com>
2022-07-26 18:57:05 +0200

    dodano nowe ekrany

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() {
   runApp(const MyApp());
